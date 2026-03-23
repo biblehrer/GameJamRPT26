5eb86ad003f57fca908395018f7533e8c0351c0c
Zenova <154024460+Voice45@users.noreply.github.com>
2026-03-23 13:59:55 +0100

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,35 +7,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 1;
 
-    private Sprite facingRight;
-    private Sprite facingLeft;
-    public Camera cam;
-    private SpriteRenderer sr;
-
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    void Awake()
-    {
-        sr = GetComponent<SpriteRenderer>();
-        cam = Camera.main; // or drag-assign via [SerializeField]
-    }
-
     // Update is called once per frame
     void Update()
     {
-        // Rotate Player here:
-           Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        if (mouseWorld.x >= transform.position.x)
-            sr.sprite = facingRight;
-        else
-            sr.sprite = facingLeft;
-
-
         // Movment here:
         if (Input.GetKey(KeyCode.D))
         {
