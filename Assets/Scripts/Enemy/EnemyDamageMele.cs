28eb7d214d7c4768b736d87b412914f76bc67789
Janaklimke <janaklimke73@gmail.com>
2026-03-23 15:03:50 +0100

    Enemy Damage
    Mele Damage vom Enemy,
    Enemy Prefab geupdated

---
+++ Assets/Scripts/Enemy/EnemyDamageMele.cs
@@ -0,0 +1,40 @@
+// EnemyDamageMele.cs
+using UnityEngine;
+
+public class EnemyDamageMele : MonoBehaviour
+{
+    private GameObject player;
+
+    public float attackDistance = 2f;
+    public float damageAmount = 10f;
+    public float attackCooldown = 2f;  // seconds between attacks
+    private float lastAttackTime = -Mathf.Infinity;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        Debug.Log("Distanz: " + distanceToPlayer + " | attackDistance: " + attackDistance);
+
+        if (distanceToPlayer <= attackDistance)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if (Time.time < lastAttackTime + attackCooldown) return;
+        lastAttackTime = Time.time;
+        playerhealth playerHealth = player.GetComponent<playerhealth>();
+
+        if (playerHealth != null)    
+        {
+            playerHealth.health -= (int)damageAmount;
+        }
+    }
+}
