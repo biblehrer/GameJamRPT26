8675dfb4137979bd5e8a621edd491633b7dec659
jannisfps <jannis.flake@online.de>
2026-03-24 11:06:40 +0100

    Attack

--- Assets/Scripts/Player/AttackHitBox.cs
+++ Assets/Scripts/Player/AttackHitBox.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class AttackHitBox : MonoBehaviour
 {   
     public int damage = 10;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "enemy")
+        if (other.tag == "enemy") //enemy tag ??
         {
             EnemyStats enemyStats = other.GetComponent<EnemyStats>();
             if (enemyStats == null)
