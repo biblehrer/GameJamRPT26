de3deb243d5bbaa69431b2349f08245f489fb3a0
Zenova <154024460+Voice45@users.noreply.github.com>
2026-03-24 10:01:20 +0100

    Player Prefabe

---
+++ Assets/Scripts/HPBarLogic.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarLogic : MonoBehaviour
+{
+    public playerhealth PlayerHp;
+    public Slider slider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HPBar();
+    }
+
+        void HPBar()
+    {
+        slider.maxValue = PlayerHp.maxHealth;
+        slider.value = PlayerHp.health;
+    }
+
+}
