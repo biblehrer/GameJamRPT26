0978d3cda44ee115aabbb1426481e41b8cd1a6ce
JuuzoG <58308992+JuuzoG@users.noreply.github.com>
2026-03-24 10:38:46 +0100

    Nichts
    meowowmwowowowowowow
    nicht funktionieredndes Shootscript

---
+++ Assets/Scripts/RangedEnemy.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 3f;
+    public float rotateSpeed = 0.0025f;
+    private Rigidbody2D rb;
+
+    public float distanceToShoot = 5f;
+    public float distanceToStop = 3f;
+
+    public float fireRate;
+    public float fireTime;
+
+    public Transform firingPoint;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+       if (!target)
+        {
+            GetTarget();
+        }
+       else
+        {
+            RotateTowardsTarget();
+        }
+       if (Vector2.Distance(target.position, transform.position) <= distanceToStop)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (fireTime <= 0f)
+        {
+            Debug.Log("Pew Pew");
+            fireTime = fireRate;
+        }
+        else
+        {
+            fireTime -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Vector2.Distance(target.position, transform.position) >= distanceToStop)
+        {
+            rb.linearVelocity = transform.up * speed;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        Vector2 targetDirection = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.Euler(new Vector3(0, 0 ,angle));
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
+    }
+
+    private void GetTarget()
+    {
+        if (GameObject.FindGameObjectWithTag("Player"))
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+            target = null;
+        }
+        else if (other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
