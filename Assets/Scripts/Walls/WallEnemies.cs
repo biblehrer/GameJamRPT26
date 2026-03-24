11bf6c076558e3c297402f024a30d5e0966c872e
Janaklimke <janaklimke73@gmail.com>
2026-03-24 14:33:49 +0100

    Enemy Dmg + Wall
    stuff

--- Assets/Scripts/Walls/WallEnemies.cs
+++ Assets/Scripts/Walls/WallEnemies.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class WallEnemies : MonoBehaviour
 {
+    // bearbeiten wenn spawner fertig ist
+    public float EnemyAmount = 10;
+
     void Start()
     {
         
@@ -9,6 +12,9 @@ public class WallEnemies : MonoBehaviour
 
     void Update()
     {
-        
+        if (EnemyAmount == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
