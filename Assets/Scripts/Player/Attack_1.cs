80d9d9426c52db8989748f9d95b32629bc304eb5
Zenova <154024460+Voice45@users.noreply.github.com>
2026-03-25 13:46:18 +0100

    PickUp Script

--- Assets/Scripts/Player/Attack_1.cs
+++ Assets/Scripts/Player/Attack_1.cs
@@ -26,8 +26,8 @@ public class Attack_1 : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Attack();
                 StartAttack();
+                Attack();
             }
         }
 
