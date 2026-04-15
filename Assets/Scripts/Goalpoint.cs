cf3ed3572d28f382c013634efe8a21a32f4a365e
JuuzoG <58308992+JuuzoG@users.noreply.github.com>
2026-04-15 12:07:58 +0200

    updated reload

--- Assets/Scripts/Goalpoint.cs
+++ Assets/Scripts/Goalpoint.cs
@@ -1,35 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
 
 public class Goalpoint : MonoBehaviour
 {
     [Header("Game UI")]
     public GameObject victoryCanvas;
     public GameObject gameOverCanvas;
-
     public PlayerStats playerHealth;
+
     private bool isGameOver = false;
+
     private void Start()
     {
+        Time.timeScale = 1f;
+
         if (victoryCanvas != null)
             victoryCanvas.SetActive(false);
-
         if (gameOverCanvas != null)
             gameOverCanvas.SetActive(false);
     }
 
     private void Update()
     {
-        Debug.Log("Health: " + playerHealth.health + " | Reload: " + Reload);
-
         if (playerHealth != null && playerHealth.health <= 0 && !isGameOver)
         {
             isGameOver = true;
             ShowGameOverScreen();
         }
-
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -38,28 +37,31 @@ public class Goalpoint : MonoBehaviour
             ShowVictoryScreen();
         }
     }
+
     private void ShowVictoryScreen()
     {
         if (victoryCanvas != null)
         {
             victoryCanvas.SetActive(true);
-            Time.timeScale = 0f; //Pause game
+            Time.timeScale = 0f;
         }
     }
+
     public void ShowGameOverScreen()
     {
-        if (isGameOver && gameOverCanvas != null)
+        if (gameOverCanvas != null)
         {
             gameOverCanvas.SetActive(true);
             Time.timeScale = 0f;
         }
     }
+
     public void PlayAgain()
     {
+        Time.timeScale = 1f; // unpausen
         isGameOver = false;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        Time.timeScale = 1f; //unpause
         Reload++;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // laden
     }
 
     public static int Reload = 0;
