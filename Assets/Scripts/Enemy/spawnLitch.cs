5de97f7427bf3fa8ce95d9786b58984cacfe1748
Zenova <154024460+Voice45@users.noreply.github.com>
2026-04-15 09:54:47 +0200

    somthing new
    I forgot what i did yesteday

---
+++ Assets/Scripts/Enemy/spawnLitch.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class spawnLitch : MonoBehaviour
+{
+public GameObject Litch;
+    bool playerInRange = false;
+    public GameObject Info;
+
+
+    void Update()
+    {
+        if(playerInRange && Input.GetButton("ActionButten"));
+    }
+
+    void SpawnMonster()
+    {
+        Instantiate(Litch);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            Info.SetActive(true);
+        }
+        else Info.SetActive(false);
+    }
+}
